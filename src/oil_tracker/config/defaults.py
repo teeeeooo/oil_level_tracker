d61d34723d6b6a7948b3a9853402bab1c2fd2ff1
teeeeooo <279387763+teeeeooo@users.noreply.github.com>
2026-07-20 16:42:27 +0900

    style: widen default workbench layout

--- src/oil_tracker/config/defaults.py
+++ src/oil_tracker/config/defaults.py
@@ -1,3 +1,3 @@
 PREVIEW_DEBOUNCE_MS = 180
-DEFAULT_WINDOW_SIZE = (1500, 900)
-SUPPORTED_VIDEO_FILTER = "Video (*.mp4 *.mkv *.avi *.mov);;All files (*)"
+DEFAULT_WINDOW_SIZE = (1600, 920)
+SUPPORTED_VIDEO_FILTER = "영상 파일 (*.mp4 *.mkv *.avi *.mov);;모든 파일 (*)"
