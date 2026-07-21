ac3f4dc1adcc55439319635d2d2f4da7016bf9dd
teeeeooo <279387763+teeeeooo@users.noreply.github.com>
2026-07-21 11:09:27 +0900

    feat: integrate truth markers with result viewer

--- src/oil_tracker/ui/redetection_result_review_window.py
+++ src/oil_tracker/ui/redetection_result_review_window.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+from dataclasses import replace
+
 from PySide6.QtCore import Signal
 from PySide6.QtGui import QAction
 from PySide6.QtWidgets import QToolBar
 
+from oil_tracker.domain.review import ReviewGraphTruthMarker
 from oil_tracker.ui.result_review_window import ResultReviewWindow
 
 
@@ -14,12 +17,15 @@ class RedetectionResultReviewWindow(ResultReviewWindow):
     selectedGlassChanged = Signal(str)
     redetectionRequested = Signal()
     truthRequested = Signal()
+    truthMarkerRequested = Signal(str)
     viewerClosing = Signal()
 
     def __init__(self, *args, **kwargs) -> None:
         self._bundle_change_guards = []
         self._glass_change_guards = []
         self._close_guards = []
+        self._truth_annotations = ()
+        self._selected_truth_annotation_id = ""
         super().__init__(*args, **kwargs)
 
     def _build_ui(self) -> None:
@@ -34,6 +40,9 @@ class RedetectionResultReviewWindow(ResultReviewWindow):
         self.truth_action.setEnabled(False)
         self.redetection_action.triggered.connect(self.redetectionRequested)
         self.truth_action.triggered.connect(self.truthRequested)
+        self.graph.truthMarkerClicked.connect(
+            lambda annotation_id, _timestamp: self.truthMarkerRequested.emit(annotation_id)
+        )
 
     def add_bundle_change_guard(self, callback) -> None:
         if callback not in self._bundle_change_guards:
@@ -53,6 +62,8 @@ class RedetectionResultReviewWindow(ResultReviewWindow):
         self.bundleAboutToChange.emit()
         loaded = super().load_bundle(source)
         if loaded:
+            self._truth_annotations = ()
+            self._selected_truth_annotation_id = ""
             enabled = self.bundle is not None
             self.redetection_action.setEnabled(enabled)
             self.truth_action.setEnabled(enabled)
@@ -86,6 +97,49 @@ class RedetectionResultReviewWindow(ResultReviewWindow):
             return False
         self.navigation.glass_combo.setCurrentIndex(index)
         return self.selected_glass_id == glass_id
+
+    def set_truth_annotations(self, annotations, *, selected_id: str = "") -> None:
+        self._truth_annotations = tuple(annotations)
+        self._selected_truth_annotation_id = str(selected_id or "")
+        self._rebuild_graph()
+        self._refresh_markers()
+
+    def _truth_markers(self) -> tuple[ReviewGraphTruthMarker, ...]:
+        if self.bundle is None or not self.selected_glass_id:
+            return ()
+        return tuple(
+            ReviewGraphTruthMarker(
+                timestamp_sec=annotation.actual_decoded_timestamp_sec,
+                annotation_id=annotation.annotation_id,
+                disposition=annotation.disposition.value,
+                error_types=tuple(value.value for value in annotation.error_types),
+                selected=annotation.annotation_id == self._selected_truth_annotation_id,
+            )
+            for annotation in self._truth_annotations
+            if annotation.glass_id == self.selected_glass_id
+            and self.bundle.analysis_start_sec
+            <= annotation.actual_decoded_timestamp_sec
+            <= self.bundle.analysis_end_sec
+        )
+
+    def _rebuild_graph(self) -> None:
+        super()._rebuild_graph()
+        if self.graph.model is not None:
+            self.graph.set_model(
+                replace(self.graph.model, truth_markers=self._truth_markers())
+            )
+
+    def _refresh_markers(self) -> None:
+        super()._refresh_markers()
+        if self.bundle is None:
+            return
+        metadata = self.playback.metadata or self.bundle.source_metadata
+        duration = (
+            metadata.duration_sec
+            if metadata is not None
+            else max(self.bundle.analysis_end_sec, 0.0)
+        )
+        self.transport.slider.set_truth_markers(duration, self._truth_markers())
 
     def closeEvent(self, event) -> None:
         if not self._allow(self._close_guards):
